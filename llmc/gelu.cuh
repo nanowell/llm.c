aae6ca55d5c3a18f092ec720786b98d2788793b6
nanowell <zarugeos@gmail.com>
2024-08-11 16:44:20 +0300

    Update gelu.cuh

--- llmc/gelu.cuh
+++ llmc/gelu.cuh
@@ -10,14 +10,17 @@
 // CUDA kernels
 
 #define GELU_SCALING_FACTOR sqrtf(2.0f / M_PI)
-__global__ void gelu_forward_kernel2(floatX* out, const floatX* inp) {
+#define GELU_COEF (GELU_SCALING_FACTOR * 0.044715f)
+#define GELU_CUBE_COEF (3.0f * 0.044715f)
+
+__global__ void gelu_forward_kernel(floatX* out, const floatX* inp) {
     int idx = (blockIdx.x * blockDim.x + threadIdx.x) * x128::size;
 
     x128 packed_out;
     x128 packed_inp = load128cs(inp + idx); // load and do not keep in cache
     for(int k = 0; k < packed_inp.size; ++k) {
         float xi = (float)packed_inp[k];
-        float cube = 0.044715f * xi * xi * xi;
+        float cube = GELU_COEF * xi * xi * xi;
         packed_out[k] = (floatX)(0.5f * xi * (1.0f + tanhf(GELU_SCALING_FACTOR * (xi + cube))));
     }
     // store instead of storecs (without cache streaming) in case it is useful for the
@@ -33,12 +36,12 @@ __global__ void gelu_backward_inplace_kernel(floatX* d_in_out, const floatX* inp
     x128 packed_dout = load128(d_in_out + idx);
     for (int k = 0; k < packed_inp.size; ++k) {
         float x = (float)packed_inp[k];
-        float cube = 0.044715f * x * x * x;
+        float cube = GELU_COEF * x * x * x;
         float tanh_arg = GELU_SCALING_FACTOR * (x + cube);
         float tanh_out = tanhf(tanh_arg);
         float coshf_out = coshf(tanh_arg);
         float sech_out = 1.0f / (coshf_out * coshf_out);
-        float local_grad = 0.5f * (1.0f + tanh_out) + x * 0.5f * sech_out * GELU_SCALING_FACTOR * (1.0f + 3.0f * 0.044715f * x * x);
+        float local_grad = 0.5f * (1.0f + tanh_out) + x * 0.5f * sech_out * GELU_SCALING_FACTOR * (1.0f + GELU_CUBE_COEF * x * x);
         packed_dinp[k] = (floatX)(local_grad * (float)packed_dout[k]);
     }
     store128(d_in_out + idx, packed_dinp);
@@ -49,15 +52,17 @@ __global__ void gelu_backward_inplace_kernel(floatX* d_in_out, const floatX* inp
 
 void gelu_forward(floatX* out, const floatX* inp, int N, cudaStream_t stream) {
     NVTX_RANGE_FN();
+    if (N <= 0) return;
     const int block_size = 512;
     assert(N % (block_size * x128::size) == 0);
     const int grid_size = CEIL_DIV(N, block_size * x128::size);
-    gelu_forward_kernel2<<<grid_size, block_size, 0, stream>>>(out, inp);
+    gelu_forward_kernel<<<grid_size, block_size, 0, stream>>>(out, inp);
     cudaCheck(cudaGetLastError());
 }
 
 void gelu_backward_inplace(floatX* d_in_out, const floatX* inp, const int N, cudaStream_t stream) {
     NVTX_RANGE_FN();
+    if (N <= 0) return;
     const int block_size = 128;
     assert(N % (block_size * x128::size) == 0);
     const int grid_size = CEIL_DIV(N, block_size * x128::size);
